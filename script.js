4babeac160603e76681aa40cd3c9b665f74c29a0
Ryan Zayne <phoenixastro97@gmail.com>
2022-10-06 23:36:34 +0100

    added input clear functionality

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const inputEl = document.getElementById("input-el"),
 	massCard = document.querySelectorAll(".result-holder")[2];
 	
 buttonEl.addEventListener("click", conversion);  
-
+inputEl.addEventListener("input", clear)
 
 function conversion() {
 	const meterConv = inputEl.value * 3.28,
@@ -14,8 +14,9 @@ function conversion() {
 		gallonConv = inputEl.value / 0.264,
 		kilogramConv = inputEl.value * 2.204,
 		poundsConv = inputEl.value / 2.204;
-		console.dir(inputEl.value)
-	if (inputEl.value > 0) {
+
+
+	if (inputEl.value > 0 ) {
 		lengthCard.textContent = `${inputEl.value} meters = ${meterConv.toFixed(3)} feet 
 									|
 									${inputEl.value} feet = ${feetConv.toFixed(3)} meters`;
@@ -27,5 +28,15 @@ function conversion() {
 		massCard.textContent = `${inputEl.value} kilograms = ${kilogramConv.toFixed(3)} pounds 
 								|
 								${inputEl.value} pounds = ${poundsConv.toFixed(3)} kilograms`;
+	} else {
+		inputEl.value = ""
+	}
+
+}
+
+
+function clear() {
+	if (inputEl.value == "") {
+		massCard.textContent = volumeCard.textContent = lengthCard.textContent = "";
 	}
 }
