2dc60f73e34ceb748318cb42e3d6da5ccb1416a1
Ryan Zayne <phoenixastro97@gmail.com>
2022-10-06 22:35:42 +0100

    few chnagees

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
 const inputEl = document.getElementById("input-el"),
-  buttonEl = document.getElementById("convert-btn"),
-  lengthCard = document.getElementsByClassName("result-holder")[0],
-  volumeCard = document.getElementsByClassName("result-holder")[1],
-  massCard = document.getElementsByClassName("result-holder")[2];
+	buttonEl = document.getElementById("convert-btn"),
+	lengthCard = document.querySelectorAll(".result-holder")[0],
+	volumeCard = document.querySelectorAll(".result-holder")[1],
+	massCard = document.querySelectorAll(".result-holder")[2];
+	
+buttonEl.addEventListener("click", conversion);  
+
 
 function conversion() {
-  const meterConv = inputEl.value * 3.28,
-    feetConv = inputEl.value / 3.28,
-    literConv = inputEl.value * 0.264,
-    gallonConv = inputEl.value / 0.264,
-    kilogramConv = inputEl.value * 2.204,
-    poundsConv = inputEl.value / 2.204;
+	const meterConv = inputEl.value * 3.28,
+		feetConv = inputEl.value / 3.28,
+		literConv = inputEl.value * 0.264,
+		gallonConv = inputEl.value / 0.264,
+		kilogramConv = inputEl.value * 2.204,
+		poundsConv = inputEl.value / 2.204;
+		console.dir(inputEl.value)
+	if (inputEl.value > 0) {
+		lengthCard.textContent = `${inputEl.value} meters = ${meterConv.toFixed(3)} feet 
+									|
+									${inputEl.value} feet = ${feetConv.toFixed(3)} meters`;
+		
+		volumeCard.textContent = `${inputEl.value} liters = ${literConv.toFixed(3)} gallons 
+									|
+									${inputEl.value} gallons = ${gallonConv.toFixed(3)} liters`;
 
-  if (inputEl.value > 0) {
-    lengthCard.textContent = `${inputEl.value} meters = ${meterConv.toFixed(3)} feet | ${inputEl.value} feet = ${feetConv.toFixed(3)} meters`;
-
-    volumeCard.textContent = `${inputEl.value} liters = ${literConv.toFixed(3)} gallons | ${inputEl.value} gallons = ${gallonConv.toFixed(3)} liters`;
-
-    massCard.textContent = `${inputEl.value} kilograms = ${kilogramConv.toFixed(3)} pounds | ${inputEl.value} pounds = ${poundsConv.toFixed(3)} kilograms`;
-  } else {
-    window.location.reload();
-  }
+		massCard.textContent = `${inputEl.value} kilograms = ${kilogramConv.toFixed(3)} pounds 
+								|
+								${inputEl.value} pounds = ${poundsConv.toFixed(3)} kilograms`;
+	}
 }
-
-buttonEl.addEventListener("click", conversion);
