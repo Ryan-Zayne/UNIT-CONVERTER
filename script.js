53efffb6f351f3d0161fe60bcd38844ff2f658bb
Ryan-Zayne <phoenixastro97@gmail.com>
2022-10-25 23:57:21 +0100

    update name

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const inputEl = document.getElementById("input-el"),
 	lengthCard = document.querySelectorAll(".result-holder")[0],
 	volumeCard = document.querySelectorAll(".result-holder")[1],
 	massCard = document.querySelectorAll(".result-holder")[2];
-	
-buttonEl.addEventListener("click", conversion);  
-inputEl.addEventListener("input", clear)
+
+buttonEl.addEventListener("click", conversion);
+inputEl.addEventListener("input", clear);
 
 
 /**
@@ -19,11 +19,11 @@ function conversion() {
 		kilogramConv = inputEl.value * 2.204,
 		poundsConv = inputEl.value / 2.204;
 
-	if (inputEl.value > 0 ) {
+	if (inputEl.value > 0) {
 		lengthCard.textContent = `${inputEl.value} meters = ${meterConv.toFixed(3)} feet 
 									|
 									${inputEl.value} feet = ${feetConv.toFixed(3)} meters`;
-		
+
 		volumeCard.textContent = `${inputEl.value} liters = ${literConv.toFixed(3)} gallons 
 									|
 									${inputEl.value} gallons = ${gallonConv.toFixed(3)} liters`;
@@ -32,7 +32,7 @@ function conversion() {
 								|
 								${inputEl.value} pounds = ${poundsConv.toFixed(3)} kilograms`;
 	} else {
-		inputEl.value = ""
+		inputEl.value = "";
 	}
 
 }
@@ -41,7 +41,7 @@ function conversion() {
 /**
  * Clears the cards if the input is empty.
  */
-function clear() {
+function clearCards() {
 	if (inputEl.value == "") {
 		massCard.textContent = volumeCard.textContent = lengthCard.textContent = "";
 	}
